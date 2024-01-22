9a9392fbc0dd7c0b6a43b8f0ee982c67fb1d95a5
Maksym Malichenko <s221680@student.uek.krakow.pl>
2024-01-23 00:23:41 +0100

    json parsing and SqlRead upgraded

---
+++ src/main/java/com/project/youtubestats/dataTypeObjects/Observation.java
@@ -0,0 +1,11 @@
+package com.project.youtubestats.dataTypeObjects;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Observation {
+  private String dateOfObservation;
+  private int valueOfObservation;
+}
--- src/main/java/com/project/youtubestats/database/MySqlRead.java
+++ src/main/java/com/project/youtubestats/database/MySqlRead.java
@@ -1,11 +1,14 @@
 package com.project.youtubestats.database;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.project.youtubestats.dataTypeObjects.Channel;
+import com.project.youtubestats.dataTypeObjects.Observation;
 
-import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+
+import static com.project.youtubestats.json.json.*;
 
 public class MySqlRead extends MySql {
 
@@ -35,20 +38,8 @@ public class MySqlRead extends MySql {
          ResultSet rs = ps.executeQuery()) {
 
       while (rs.next()) {
-       String id = rs.getString("id");
-       String channelLink = rs.getString("channel_link");
-       String channelUserAlias = rs.getString("channel_user_alias");
-       String channelTitle = rs.getString("channel_title");
-       int collectOnceInNDays = rs.getInt("collect_once_in_n_days");
-       boolean collectVideoCount = rs.getBoolean("collect_video_count");
-       boolean collectViewCount = rs.getBoolean("collect_view_count");
-       boolean collectSubscriberCount = rs.getBoolean("collect_subscriber_count");
-       String startCollectFrom = rs.getString("start_collect_from");
-       boolean onPause = rs.getBoolean("on_pause");
-       String userComment = rs.getString("user_comment");
 
-       Channel channel = new Channel(id,channelLink,channelUserAlias,channelTitle,collectOnceInNDays,collectVideoCount,collectViewCount,collectSubscriberCount,startCollectFrom,onPause,userComment);
-       channels.add(channel);
+        channels.add(createChannelFromSqlOutputLine(rs));
       }
 
     } catch (SQLException e) {
@@ -57,13 +48,80 @@ public class MySqlRead extends MySql {
     return channels;
   }
 
- /* public static Channel getChannel() throws IOException {
+  public static Channel getChannelByLink(String givenChannelLink){
 
-    return
-  } */
+    String sqlStatement = "SELECT * FROM channel where channel_link='"+givenChannelLink+"';";
+    try (Connection conn = DriverManager.getConnection(connectionUrl, "admin", "youtubestats");
+
+         PreparedStatement ps = conn.prepareStatement(sqlStatement);
+         ResultSet rs = ps.executeQuery()) {
+
+      while (rs.next()) {
+
+        Channel channel = createChannelFromSqlOutputLine(rs);;
+        return channel;
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return new Channel();
+  }
+
+  private static Channel createChannelFromSqlOutputLine(ResultSet rs) throws SQLException {
+    String id = rs.getString("id");
+    String channelLink = rs.getString("channel_link");
+    String channelUserAlias = rs.getString("channel_user_alias");
+    String channelTitle = rs.getString("channel_title");
+    int collectOnceInNDays = rs.getInt("collect_once_in_n_days");
+    boolean collectVideoCount = rs.getBoolean("collect_video_count");
+    boolean collectViewCount = rs.getBoolean("collect_view_count");
+    boolean collectSubscriberCount = rs.getBoolean("collect_subscriber_count");
+    String startCollectFrom = rs.getString("start_collect_from");
+    boolean onPause = rs.getBoolean("on_pause");
+    String userComment = rs.getString("user_comment");
+
+    Channel channel = new Channel(id,channelLink,channelUserAlias,channelTitle,collectOnceInNDays,collectVideoCount,collectViewCount,collectSubscriberCount,startCollectFrom,onPause,userComment);
+   return channel;
+  }
+
+  public static ArrayList<Observation> getObservationsForChannel(Channel channel, observationType observationType){
+    ArrayList<Observation> observations= new ArrayList<>();
+    String channelId = channel.getChannelId();
+
+    String sqlStatement = "select * from channel_" +
+      observationType +
+      " cot inner join " +
+      observationType +
+      " ot on cot.id_observation=ot.id" +
+      " where id_channel='"+channelId+"';";
+    try (Connection conn = DriverManager.getConnection(connectionUrl, "admin", "youtubestats");
+
+         PreparedStatement ps = conn.prepareStatement(sqlStatement);
+         ResultSet rs = ps.executeQuery()) {
+
+      while (rs.next()) {
+        String observationDate = rs.getString("obs_date");
+        int observationValue = rs.getInt("obs_value");
+        Observation observation = new Observation(observationDate,observationValue);
+        observations.add(observation);
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return observations;
+  }
 
 
-  public static void main(String[] args) {
-    System.out.println(getLastAddedObservationId(observationType.subscriber));
+  public static void main(String[] args) throws JsonProcessingException {
+
+    //ArrayList<Channel> channels = new ArrayList<>();
+    //channels = getAllChannelsFollowed();
+    Channel channel = getChannelByLink("https://www.youtube.com/@kaiserbauch9092");
+    System.out.println(createChannelWithMetricsJson(channel));
+
+
   }
 }
