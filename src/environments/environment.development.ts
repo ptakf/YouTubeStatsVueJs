e01f4effba84a6ee2603c5932b40052dafb4f1dc
ptakf <s222693@student.uek.krakow.pl>
2024-02-03 16:41:36 +0100

    Remove Angular from the project

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  statisticsJson: JSON.stringify({}),
-  channelListJson: JSON.stringify([]),
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  statisticsJson: JSON.stringify({}),
-  channelListJson: JSON.stringify([]),
-};
